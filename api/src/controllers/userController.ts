95290e57817acaadd73d11d216d50b32058d8c0f
alberthydev <hygor.marques@protonmail.com>
2026-05-05 08:17:20 -0300

    adjusted files

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -1,26 +1,37 @@
-import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import User from '../models/User';
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User";
 
-export const registryUser = async (req: Request, res: Response): Promise<void> => {
+export const registryUser = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
   try {
-    const { cpf, matricula, senha, nome, sobrenome, fotoUrl, carros, tipo } = req.body;
+    const { cpf, matricula, senha, nome, sobrenome, fotoUrl, carros, tipo } =
+      req.body;
 
-    const regexSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const regexSenha =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!regexSenha.test(senha)) {
-      res.status(400).json({ erro: 'A senha não atende aos requisitos mínimos de segurança.' });
+      res
+        .status(400)
+        .json({
+          erro: "A senha não atende aos requisitos mínimos de segurança.",
+        });
       return;
     }
 
     const regexFoto = /\.(png|jpg|jpeg)$/i;
     if (!regexFoto.test(fotoUrl)) {
-      res.status(400).json({ erro: 'A foto deve estar no formato PNG, JPG ou JPEG.' });
+      res
+        .status(400)
+        .json({ erro: "A foto deve estar no formato PNG, JPG ou JPEG." });
       return;
     }
 
     const hasUser = await User.findOne({ cpf });
     if (hasUser) {
-      res.status(400).json({ erro: 'CPF já cadastrado.' });
+      res.status(400).json({ erro: "CPF já cadastrado." });
       return;
     }
 
@@ -35,23 +46,23 @@ export const registryUser = async (req: Request, res: Response): Promise<void> =
       sobrenome,
       tipo,
       fotoUrl,
-      carros: carros || [] // Carros é opcional
+      carros: carros || [], // Carros é opcional
     });
 
     await newUser.save();
 
-    res.status(201).json({ mensagem: 'Usuário cadastrado com sucesso!' });
+    res.status(201).json({ mensagem: "Usuário cadastrado com sucesso!" });
   } catch (erro) {
-    console.error('Erro no cadastro:', erro);
-    res.status(500).json({ erro: 'Erro interno do servidor.' });
+    console.error("Erro no cadastro:", erro);
+    res.status(500).json({ erro: "Erro interno do servidor." });
   }
 };
 
-export const usersList = async (req: Request,  res: Response): Promise<void> => {
-  try{
+export const usersList = async (req: Request, res: Response): Promise<void> => {
+  try {
     const visitantes = await User.find({});
     res.status(200).json(visitantes);
-  } catch(erro){
-    res.status(500).json({ erro: 'Erro ao buscar usuário.' });
+  } catch (erro) {
+    res.status(500).json({ erro: "Erro ao buscar usuário." });
   }
-}; 
+};
